1c95ee5f8b285409a47d7b80e07885bddde255e4
Eugene_Alll <l.k.bayete@gmail.com>
2016-07-06 09:21:44 +0300

    Change DB

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/test'
-    // 'url' : 'mongodb://Eugene_Alll:liveandfun@ec2-52-34-55-203.us-west-2.compute.amazonaws.com:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    // 'url' : 'mongodb://localhost:27017/test'
+    'url' : 'mongodb://Eugene_Alll:liveandfun@ec2-52-34-55-203.us-west-2.compute.amazonaws.com:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
